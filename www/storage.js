b4bd31544e4280e9f210ca6378d28445b8ac251e
mpallansch <matthew.pallansch@gmail.com>
2019-04-17 16:34:25 -0400

    Changes

--- www/storage.js
+++ www/storage.js
@@ -1,6 +1,7 @@
 ﻿alert('Installing plugin!');
 
 window.getPreviousStorage = function(str, callback) {
+    alert('Plugin received method call!');
     cordova.exec(callback, function(err) {
         callback('Error fetching old data.');
     }, "Storage", "getPreviousStorage", [str]);
