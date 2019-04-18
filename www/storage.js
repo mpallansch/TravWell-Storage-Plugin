43f01efd27208cc1150627410e0883dfba4a43b7
mpallansch <matthew.pallansch@gmail.com>
2019-04-18 10:11:13 -0400

    Changes

--- www/storage.js
+++ www/storage.js
@@ -4,7 +4,7 @@ var exec = require('cordova/exec');
 
 exports.getPreviousStorage = function(str, callback) {
     alert('Plugin received method call!');
-    cordova.exec(callback, function(err) {
+    exec(callback, function(err) {
         callback('Error fetching old data. ' + err);
     }, "Storage", "getPreviousStorage", [str]);
 }
