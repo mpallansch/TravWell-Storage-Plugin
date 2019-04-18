ddbd87385893916b3ef87090f6da4aedb1092d83
mpallansch <matthew.pallansch@gmail.com>
2019-04-18 10:02:37 -0400

    Changes

--- src/android/Storage.java
+++ src/android/Storage.java
@@ -12,7 +12,7 @@ import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.LOG;
+import android.util.Log;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONArray;
@@ -21,10 +21,25 @@ import java.util.Arrays;
 public class Storage extends CordovaPlugin {
 
     @Override public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("getPreviousStorage")) { 
-            String message = args.getString(0);
-            callbackContext.success("Hello Plugin! Here's your message champ: " + message);
-            return true;
-        } return false;
+        Log.d("Foobar", "execute");
+        try {
+            Log.d("Foobar", "try");
+            if (action.equals("getPreviousStorage")) { 
+                Log.d("Foobar", "action equals getPreviousStorage");
+                String message = args.getString(0);
+                Log.d("Foobar", "message = " + message);
+                callbackContext.success("Hello Plugin! Here's your message champ: " + message);
+                Log.d("Foobar", "after callback");
+                return true;
+            } else { 
+                Log.d("Foobar", "else");
+                callbackContext.error("Action not recognized");
+                return false; 
+            }
+        } catch(Exception e) {
+            Log.d("Foobar", "catch");
+            callbackContext.error("Exception thrown");
+            return false;
+        }
     } 
 }
--- www/storage.js
+++ www/storage.js
@@ -1,9 +1,10 @@
 ﻿alert('Installing plugin!');
 
-window.getPreviousStorage = function(str, callback) {
+var exec = require('cordova/exec');
+
+exports.getPreviousStorage = function(str, callback) {
     alert('Plugin received method call!');
     cordova.exec(callback, function(err) {
-        callback('Error fetching old data.');
+        callback('Error fetching old data. ' + err);
     }, "Storage", "getPreviousStorage", [str]);
-};
- 
+}
