a932db59a4e41c0610a3a605d090e84a11134317
mpallansch <matthew.pallansch@gmail.com>
2019-04-22 12:47:02 -0400

    Adds Select Code and Manifest Tags

--- src/android/Storage.java
+++ src/android/Storage.java
@@ -17,29 +17,55 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONException;
 import org.json.JSONArray;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.activeandroid.query.Select;
 import com.activeandroid.ActiveAndroid;
+
+import gov.cdc.oid.ncezid.travwell.model.*;
 
 public class Storage extends CordovaPlugin {
 
     @Override public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        try {
-            if (action.equals("getPreviousStorage")) { 
+        if (action.equals("getPreviousStorage")) { 
+            try {
                 ActiveAndroid.initialize(this.cordova.getActivity().getApplicationContext());
                 
-                String message = args.getString(0);
-                callbackContext.success(message);
+                List<Alarm> alarms = new Select().from(Alarm.class).execute();
+                List<Destination> destinations = new Select().from(Destination.class).execute();
+                List<Disease> diseases = new Select().from(Disease.class).execute();
+                List<Document> documents = new Select().from(Document.class).execute();
+                List<Drug> drugs = new Select().from(Drug.class).execute();
+                List<PackingGroup> packingGroups = new Select().from(PackingGroup.class).execute();
+                List<PackingItem> packingItems = new Select().from(PackingItem.class).execute();
+                List<PackingSuperGroup> packingSuperGroups = new Select().from(PackingSuperGroup.class).execute();
+                List<Profile> profiles = new Select().from(Profile.class).execute();
+                List<Trip> trips = new Select().from(Trip.class).execute();
+                
+                callbackContext.success(
+                    alarms.toString() + "\n" + 
+                    destinations.toString() + "\n" + 
+                    diseases.toString() + "\n" + 
+                    documents.toString() + "\n" + 
+                    drugs.toString() + "\n" + 
+                    packingGroups.toString() + "\n" + 
+                    packingItems.toString() + "\n" + 
+                    packingSuperGroups.toString() + "\n" + 
+                    profiles.toString() + "\n" + 
+                    trips.toString() + "\n"
+                );
                 
                 ActiveAndroid.dispose();
                 return true;
-            } else { 
-                callbackContext.error("Action not recognized");
-                return false; 
+            } catch(Exception e) {
+                e.printStackTrace();
+                callbackContext.error("Exception thrown " + e.getMessage());
+                return false;
             }
-        } catch(Exception e) {
-            e.printStackTrace();
-            callbackContext.error("Exception thrown " + e.getMessage());
-            return false;
+        } else { 
+            callbackContext.error("Action not recognized");
+            return false; 
         }
     } 
 }
