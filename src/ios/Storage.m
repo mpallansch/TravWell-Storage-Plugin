f345718eceee7e85994457602b6b141ea136dc3c
mpallansch <matthew.pallansch@gmail.com>
2019-05-03 12:57:07 -0400

    Adds RK to Pods and Adds MagicalRecord Init Code

--- src/ios/Storage.m
+++ src/ios/Storage.m
@@ -1,5 +1,6 @@
 #import "Storage.h"
 #import "TSApi/Models/CoreData/TSDisease.h"
+#import <MagicalRecord/MagicalRecord.h>
 #import <Cordova/CDVPlugin.h>
 
 @implementation Storage
@@ -8,6 +9,33 @@
 {
     CDVPluginResult* pluginResult = nil;
     NSString* message = [command.arguments objectAtIndex:0];
+
+    [MagicalRecord enableShorthandMethods];
+
+// Setup CoreData with RestKit+ MagicalRecord
+    NSURL* modelURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"TSCoreDataModels"
+                                                                             ofType:@"momd"]];
+    NSManagedObjectModel* managedObjectModel = [[[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL] mutableCopy];
+    RKManagedObjectStore* managedObjectStore = [[RKManagedObjectStore alloc] initWithManagedObjectModel:managedObjectModel];
+    NSString* storePath = [RKApplicationDataDirectory() stringByAppendingPathComponent:@"RKMagicalRecord.sqlite"];
+    NSError* error = nil;
+
+    [managedObjectStore addSQLitePersistentStoreAtPath:storePath
+                                fromSeedDatabaseAtPath:nil
+                                     withConfiguration:nil
+                                               options:nil
+                                                 error:&error];
+    [managedObjectStore createManagedObjectContexts];
+
+    // (hopefully?) Configure MagicalRecord to use RestKit's Core Data stack
+    [NSPersistentStoreCoordinator MR_setDefaultStoreCoordinator:managedObjectStore.persistentStoreCoordinator];
+    [NSManagedObjectContext MR_setRootSavingContext:managedObjectStore.persistentStoreManagedObjectContext];
+    [NSManagedObjectContext MR_setDefaultContext:managedObjectStore.mainQueueManagedObjectContext];
+
+    [RKManagedObjectStore setDefaultStore:managedObjectStore];
+    self.sharedManagedObjectContext = managedObjectStore.mainQueueManagedObjectContext;
+    [TSObjectMapping configureObjectManagerWithStore:managedObjectStore];
+    [MagicalRecord setupAutoMigratingCoreDataStack];
 
     NSArray* diseases = [TSDisease MR_findAll];
 
